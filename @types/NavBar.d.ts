5311f8d5d773387b722f05de3dd667f85b1180ee
张铂晗 <zhangbh@xinfujinan.com>
2019-08-05 12:21:11 +0800

    first commit

---
+++ @types/NavBar.d.ts
@@ -0,0 +1,21 @@
+import { ComponentClass } from "react";
+
+export interface NavBarProps {
+  extClass: string;
+  title: string;
+  background: string;
+  color: string;
+  back: boolean;
+  loading: boolean;
+  animated: boolean;
+  show: boolean;
+  delta: number;
+  displayStyle: string;
+  isNative: boolean;
+  renderRight: () => any;
+  renderLeft: () => any;
+}
+
+declare const NavBar: ComponentClass<NavBarProps>;
+
+export default NavBar;
